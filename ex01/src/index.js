30d2ac1c47eb68feb1ca07540818f81cf9089890
krehicbenjamin <krehicbenjamin@gmail.com>
2021-03-20 08:08:05 +0100

    adding first task'

---
+++ ex01/src/index.js
@@ -0,0 +1,13 @@
+
+function myRecursion(arr, n) {
+    var sum = 0;
+    if (n <= 0) {
+        return 0;
+    } 
+    sum = myRecursion(arr, n - 1) + arr[n - 1];
+    return sum;
+}
+
+console.log(myRecursion([1, 2, 3, 4], 2));
+console.log(myRecursion([1, 2, 3, 4], 3));
+module.exports = myRecursion;
